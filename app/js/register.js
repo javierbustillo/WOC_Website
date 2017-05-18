d0b0be46e40ff1ddaaec19240b73d71bb7fb3f4d
rotcehxp <hector.diaz6@upr.edu>
2017-05-18 18:46:34 -0400

    Comments were added

--- app/js/register.js
+++ app/js/register.js
@@ -2,53 +2,43 @@ window.onload = function(){
 
   //Create reference to the create button
   var createButton = document.getElementById("createButton");
-
   //Create reference to the sign in button
   var signin_button = document.getElementById("signin_button");
-
+  
   //Check if the create button is pressed
   createButton.onclick = function(){
-  	
   	//Get email input
  	var email = document.getElementById("email").value;
-
  	//Get password input
   	var password = document.getElementById("password").value;
-
   	firebase.auth().createUserWithEmailAndPassword(email, password).then(function(result){ 
   		console.log('result', result);
-  		// User is signed in.
+  		// Executed when the user is signed in.
 		if (result) {
-		  
 		  //Get name input
 		  var user_displayName = document.getElementById("name").value;
-		  
 		  //Update user display name with the name input obtained
 		  result.updateProfile({displayName: user_displayName});
-		  
 		  //Create an object with all the attributes of the user
 		  var user_info = {id: result.uid, email: result.email, display_name: user_displayName };
-		  
 		  //Declare database
 		  var database = firebase.database();
-
 		  //Create a user in the firebase data-base
 		  database.ref().child('users/').push({
 		      display_name: user_info.display_name,
 		      id: user_info.id,
 		      email: user_info.email
-		   //After executed, redirects to index.html
+		   //Executed after the database have been updates,
+		   //redirects to index.html
 		   }).then(function(){
 		   		location.href = "index.html";
 		   });;
-
 		//Indicate if something went wrong
 		} else {
 		  alert("Something went wrong.");
 		}
   	})
     .catch(function(error) {
-
 	  //Handle errors
 	  var errorCode = error.code;
 	  var errorMessage = error.message;
