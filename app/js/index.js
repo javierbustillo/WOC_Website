70775f89c22c1a5606b3a6e1200ba3859185c788
rotcehxp <hector.diaz6@upr.edu>
2017-05-17 22:33:13 -0400

    comments

--- app/js/index.js
+++ app/js/index.js
@@ -3,24 +3,30 @@ window.onload = function(){
 
 	var singoutButton = document.getElementById('logout_button');
 
+  //Check if a user is logged in.
 	firebase.auth().onAuthStateChanged(function(user) {
 		console.log(user);
   		if (user) {
-    		// User is signed in.
+    		
     		var username = document.getElementById('username');
-			var username_obtained;
-			console.log(user);
-			if (user!=null) {
+			  var username_obtained;
+			  console.log(user);
+        //Get the name of the user.
+			  if (user!=null) {
   				username_obtained = user.displayName;
-			} else {
-				alert("Something went wrong.");
-			}
-			username.innerHTML = username_obtained;
+			  } else {
+				  alert("Something went wrong.");
+			  }
+        //Assign it to the header. 
+		  	username.innerHTML = username_obtained;
+        //If the user is not logged in, 
+        //returns it to the login page.
   		} else {
     		window.location = "login.html";
   		}
   	});
 
+    //Checks if the log out button is pressed. 
   	singoutButton.onclick = function(){
   		firebase.auth().signOut().then(function() {
   		// Sign-out successful.
