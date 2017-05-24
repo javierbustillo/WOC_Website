e337eb4197220b86f43227c7a5e5154924a42f95
Hector Enrique Diaz Morales <enriquesh1000@mbpdehezmorales.lan>
2017-05-24 15:38:01 -0400

    Submit_event.js uploads image to the storage correctly, when cancelled it deletes it.

--- app/js/submit_event.js
+++ app/js/submit_event.js
@@ -6,6 +6,7 @@ $(document).ready(function() {
     // Set up UI elements
     var publish_button = document.querySelector("#publish_button"),
         logout_button = document.querySelector("#logout_button"),
+        cancel_button = document.querySelector("#cancel_button"),
         username = document.querySelector("#username"),
         title = document.querySelector("#title"),
         date = document.querySelector("#date"),
@@ -19,7 +20,7 @@ $(document).ready(function() {
     console.log("Inputs referenced.");
 
     //Set user attributes
-    var total_counter, active_counter, user_info, user_id, user_display_name;
+    var total_counter, active_counter, user_info, user_id, user_display_name, file_name, isUploaded;
 
     //Initialize Firebase
     var auth = firebase.auth(),
@@ -42,47 +43,74 @@ $(document).ready(function() {
       if(user){
         console.log("User detected.");
         user_id = user_info.uid;
-        console.log("User id read.");
         user_display_name = user_info.displayName;
-        console.log("User display name read.");
         //Assign username to header
-        console.log(username);
         username.innerHTML = user_display_name;
         database.ref("users/"+user_id).once("value").then(function(snapshot){
           total_counter = snapshot.val().total_event_counter;
-          console.log("Total event counter read.");
           active_counter = snapshot.val().active_event_counter;
-          console.log("Active event counter read.");
         });
       }else{
         window.location = "login.html";
       }
     });
     
-    //Publish event
-    publish_button.addEventListener('click', function(e){
-      console.log("Publish button have been pressed.");
-      database.ref("users/"+user_id).update({total_event_counter: total_counter+1});
-      console.log("Total event counter incremented by 1.");
-      database.ref("users/"+user_id).update({active_event_counter: total_counter+1});
-      console.log("Active event counter incremented by 1.");
-      database.ref("events/").push({
-        title: title.value,
-        date: date.value,
-        hour: hour.value,
-        place: place.value,  
-        brief_description: brief_description.value,
-        detailed_description: detailed_description.value,
-        contact_email: contact_email.value,
-        contact_phone_number: contact_phone_number.value,
-        imageUrl: imageUrl.value,
-        user_id: user_id,
+    //Upload image to storage
+    image_input.addEventListener('change', function(e){
+      //Get image file
+      var file = e.target.files[0];
+      file_name = Math.random().toString(36).substring(15);
+
+      //Upload image to storage
+        var task = storage.ref(user_id+"/"+file_name).put(file);
+        //Update progress bar
+        task.on('state_changed',
+          function progress(snapshot){
+            var percentage = (snapshot.bytesTransferred/snapshot.totalBytes)*100;
+            console.log(percentage);
+          },
+          function error(err){
+          },
+          function complete(){
+            isUploaded=true;
+            console.log("Image has been uploaded.");
+          });
       });
-      console.log("New event created.");
-    });
+
+      //Publish event
+      publish_button.addEventListener('click', function(e){
+        console.log("Publish button have been pressed.");
+        //Increment counters
+        database.ref("users/"+user_id).update({total_event_counter: total_counter+1});
+        database.ref("users/"+user_id).update({active_event_counter: total_counter+1});
+
+        //Save event info to database and get the unique ID
+        database.ref("events/").push({
+          title: title.value,
+          date: date.value,
+          hour: hour.value,
+          place: place.value,  
+          brief_description: brief_description.value,
+          detailed_description: detailed_description.value,
+          contact_email: contact_email.value,
+          contact_phone_number: contact_phone_number.value,
+          imageUrl: user_id+"/"+file_name,
+          user_id: user_id,
+        })
+        console.log("New event created.");
+      });
+
+      //Cancel form submit
+      cancel_button.addEventListener('click', function(e){
+        if(isUploaded){
+          storage.ref(user_id+"/"+file_name).delete();
+          console.log("Image has been deleted.");
+        }
+        window.location = "submit_event.html";
+      });
+    
   });
 });
-
 
 
 
