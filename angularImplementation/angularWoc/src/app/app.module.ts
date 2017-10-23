d12f26191c5f513609270651bb73be631765b516
supremekhaoz <luisliz2038@gmail.com>
2017-10-22 23:51:54 -0400

    Multiple modules and components. Firebase, templates and authentication

--- angularImplementation/angularWoc/src/app/app.module.ts
+++ angularImplementation/angularWoc/src/app/app.module.ts
@@ -1,16 +1,54 @@
+//Angular Imports
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router'
 
-import { AppComponent } from './app.component';
+//General Imports
+import { environment } from '../environments/environment';
+import { appRouting } from "./app.routes";
+
+//Components
+import { TemplateComponent } from './shared/template/template.component';
+import { HomeComponent } from './components/home/home.component';
+import { ErrorComponent } from './components/error/error.component';
+import { LoginComponent } from './components/login/login.component';
+
+//Services
+import { AuthService } from './services/auth/auth.service';
+
+//Firebase Modules
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
 
 @NgModule({
   declarations: [
-    AppComponent
+    HomeComponent,
+    ErrorComponent,
+    LoginComponent,
+    TemplateComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule,
+    appRouting,
+    AngularFireModule.initializeApp(environment.firebase, 'whats-on-campus'),
+		AngularFireDatabaseModule,
+		AngularFireAuthModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+  	AuthService,
+  ],
+  bootstrap: [TemplateComponent]
 })
+
 export class AppModule { }
+
+
+
+
+
