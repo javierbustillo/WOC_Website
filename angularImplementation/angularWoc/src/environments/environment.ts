d264bf1b80a48bcf1fdea67c34bb12fe9603c864
supremekhaoz <luisliz2038@gmail.com>
2017-10-22 23:46:31 -0400

    Firebase enviorment variables

--- angularImplementation/angularWoc/src/environments/environment.ts
+++ angularImplementation/angularWoc/src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+		apiKey: "AIzaSyCIUFSaKhAX1z3aI3-iOqeCQGOATPP7XHY",
+		authDomain: "whats-on-campus.firebaseapp.com",
+		databaseURL: "https://whats-on-campus.firebaseio.com",
+		projectId: "whats-on-campus",
+		storageBucket: "whats-on-campus.appspot.com",
+		messagingSenderId: "2443063959"
+  }
 };
